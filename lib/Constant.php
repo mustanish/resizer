5f5f57bc9ec1e063c5e1470803b91114ce9f8114
Mustanish Altamash <mustanish.altamash@gmail.com>
2020-02-09 18:52:34 +0530

    Code Refactoring

--- lib/Constant.php
+++ lib/Constant.php
@@ -13,9 +13,9 @@
         private $allowedSize;
         private $quality;
 
-		/**
-		 * Set default values from constant file
-		 */
+        /**
+         * Set default values from constant file
+         */
         public function __construct() {	
             $this->generateThumbnail = GENERATE_THUMBNAIL;
             $this->thumbnailSize = explode(',',THUMBNAIL_SIZE);
@@ -26,11 +26,11 @@
             $this->quality = QUALITY;
         }
  
-		/**
-		 * Get the value of generateThumbnail
-		 *
-		 * @return boolean
-		 */
+        /**
+         * Get the value of generateThumbnail
+         *
+         * @return boolean
+         */
         public function getGenerateThumbnail() {
             return $this->generateThumbnail;
         }
@@ -46,121 +46,121 @@
             return $this;
         }
 
-		/**
-		 * Get the value of thumbnailSize
-		 *
-		 * @return array
-		 */
+        /**
+         * Get the value of thumbnailSize
+         *
+         * @return array
+         */
         public function getThumbnailSize() {
             return $this->thumbnailSize;
         }
 
-		/**
-		 * Set the value of thumbnailSize
-		 *
-		 * @param array $thumbnailSize
-		 * @return self
-		 */
+        /**
+         * Set the value of thumbnailSize
+         *
+         * @param array $thumbnailSize
+         * @return self
+         */
         public function setThumbnailSize(array $thumbnailSize) {
             $this->thumbnailSize = $thumbnailSize;
             return $this;
         }
 
-		/**
-		 * Get the value of thumbnailName
-		 *
-		 * @return array
-		 */
+        /**
+         * Get the value of thumbnailName
+         *
+         * @return array
+         */
         public function getthumbnailName() {
             return $this->thumbnailName;
         }
 
-		/**
-		 * Set the value of thumbnailName
-		 *
-		 * @param array $thumbnailName
-		 * @return self
-		 */
+        /**
+         * Set the value of thumbnailName
+         *
+         * @param array $thumbnailName
+         * @return self
+         */
         public function setthumbnailName(array $thumbnailName) {
             $this->thumbnailName = $thumbnailName;
             return $this;
         }
 
-		/**
-		 * Get the value of destinationFolder
-		 *
-		 * @return string
-		 */
+        /**
+         * Get the value of destinationFolder
+         *
+         * @return string
+         */
         public function getDestinationFolder() {
             return $this->destinationFolder;
         }
 
-		/**
-		 * Set the value of destinationFolder
-		 *
-		 * @param string $destinationFolder
-		 * @return self
-		 */
+        /**
+         * Set the value of destinationFolder
+         *
+         * @param string $destinationFolder
+         * @return self
+         */
         public function setDestinationFolder(string $destinationFolder) {
             $this->destinationFolder = $destinationFolder;
             return $this;
         }
 
-		/**
-		 * Get the value of maxSize
-		 *
-		 * @return integer
-		 */
+        /**
+         * Get the value of maxSize
+         *
+         * @return integer
+         */
         public function getMaxSize() {
             return $this->maxSize;
         }
  
-		/**
-		 * Set the value of maxSize
-		 *
-		 * @param integer $maxSize
-		 * @return self
-		 */
+        /**
+         * Set the value of maxSize
+         *
+         * @param integer $maxSize
+         * @return self
+         */
         public function setMaxSize(int $maxSize) {
             $this->maxSize = $maxSize;
             return $this;
         }
 
-		/**
-		 * Get the value of allowedSize
-		 *
-		 * @return integer
-		 */
+        /**
+         * Get the value of allowedSize
+         *
+         * @return integer
+         */
         public function getAllowedSize() {
             return $this->allowedSize;
         }
  
-		/**
-		 * Set the value of allowedSize
-		 *
-		 * @param integer $allowedSize
-		 * @return self
-		 */
+        /**
+         * Set the value of allowedSize
+         *
+         * @param integer $allowedSize
+         * @return self
+         */
         public function setAllowedSize(int $allowedSize) {
             $this->allowedSize = $allowedSize;
             return $this;
         }
  
-		/**
-		 * Get the value of quality
-		 *
-		 * @return integer
-		 */
+        /**
+         * Get the value of quality
+         *
+         * @return integer
+         */
         public function getQuality() {
             return $this->quality;
         }
 
-		/**
-		 * Set the value of quality
-		 *
-		 * @param integer $quality
-		 * @return self
-		 */
+        /**
+         * Set the value of quality
+         *
+         * @param integer $quality
+         * @return self
+         */
         public function setQuality(int $quality) {
             $this->quality = $quality;
             return $this;
